382369a62e08d025495c65d8ac181043851deffe
Mabongakev <kelvin.mabonga92@gmail.com>
2024-05-22 08:31:08 -0700

    updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,32 +5,34 @@ Write a Python script that uses REST API that returns information about a given 
 
 import requests
 import sys
+import json
+
+def get_employee_todos(id):
+    employee_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{id}"
+    )
+
+    if employee_response.status_code != 200:
+        return
+
+    todos_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{id}/todos")
+
+    if todos_response.status_code != 200:
+        return
+
+    name = employee_response.json().get('name')
+
+    todos = todos_response.json()
+    completed_tasks = [task for task in todos if task.get('completed') is True]
+    total_tasks = len(todos_response.json())
+
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+          len(completed_tasks), total_tasks))
+
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
+
 
 if __name__ == "__main__":
-    # Check if the script is provided with an employee ID as a command-line argument
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
-    employee_ID = sys.argv[1]
-    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
-    url = f'{jsonplaceholder}/{employee_ID}'
-
-
-    response = requests.get(url)
-
-
-    if response.status_code == 200:
-        employee_name = response.json().get('name')
-        Todourl = f'{url}/todos'
-        res = requests.get(Todourl)
-        tasks = res.json()
-
-
-        done_tasks = [task for task in tasks if task.get('completed')]
-
-
-        print("Employee {} is done with tasks({}/{}):".format(employee_name, len(done_tasks), len(tasks)))
-        for task in done_tasks:
-            print("\t{}".format(task.get('title')))
-    else:
-        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+    get_employee_todos(sys.argv[1])
