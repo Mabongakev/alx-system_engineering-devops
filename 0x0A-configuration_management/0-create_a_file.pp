2efa6609bd234c86680a173e086e49b3b99078aa
Mabongakev <kelvin.mabonga92@gmail.com>
2025-01-12 21:49:05 -0800

    revised file created

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 #create a file
 
-file { '/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet'
+file {'kev_code':
+  path    => '/tmp/school'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
